838f53cedb27e0ed36eb6a688c855e4f4577a77e
EthanTang <tangrenxing@gmail.com>
2016-10-03 16:13:26 -0500

    Added comments

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,12 +1,16 @@
 list = []
 temp = 0
 min = 0
+max = 0
 import random
-for i in range(1,100):
+# sorting 100 random numbers took 0.115s
+# sorting 10000 random numbers took 15.969s
+# sorting by moving the min value to the left ONLY
+for i in range(1,10000):
     list.append(random.randint(0,10000))
 print list
-for x in range(0,99):
-    for y in range(x,99):
+for x in range(0,9999):
+    for y in range(x,9999):
         min = list[x]
         if list[y]< min :
             temp = list[y]
